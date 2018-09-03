aa78860a7d2e7e9947313288ba11753407c8051b
iamwebwiz <iamwebwiz@gmail.com>
2018-09-03 16:37:17 +0100

    update users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('mobile')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('country')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('skype')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();

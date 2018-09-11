43e76b580f97624e2b0d19755dbacb3f487d28e3
iamwebwiz <iamwebwiz@gmail.com>
2018-09-12 00:49:46 +0100

    update listing image model

--- app/ListingImage.php
+++ app/ListingImage.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ListingImage extends Model
 {
-    //
+    public function listing()
+    {
+        return $this->belongsTo(Listing::class);
+    }
 }

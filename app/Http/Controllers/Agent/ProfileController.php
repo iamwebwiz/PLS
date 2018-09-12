25d9da77327da57806d05d2e46d750444eb9efca
iamwebwiz <iamwebwiz@gmail.com>
2018-09-12 22:37:49 +0100

    add agent profile controller

---
+++ app/Http/Controllers/Agent/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Agent;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $agent = auth()->user();
+
+        return $agent;
+    }
+}

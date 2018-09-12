87c24f4f77224edd9e8451e6f696e2a6f44e494d
iamwebwiz <iamwebwiz@gmail.com>
2018-09-12 22:42:16 +0100

    update admin listing controller

--- app/Http/Controllers/Admin/ListingController.php
+++ app/Http/Controllers/Admin/ListingController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Listing;
+use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
     public function __construct()
     {
         $this->middleware('admin');
+    }
+
+    public function index()
+    {
+        $listings = Listing::all();
+
+        return $listings;
     }
 }

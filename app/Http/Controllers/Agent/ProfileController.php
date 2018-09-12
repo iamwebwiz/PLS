391d6c6da59e53bdac5b547347b9cba4c173b940
iamwebwiz <iamwebwiz@gmail.com>
2018-09-12 22:49:58 +0100

    update agent profile controller

--- app/Http/Controllers/Agent/ProfileController.php
+++ app/Http/Controllers/Agent/ProfileController.php
@@ -9,7 +9,7 @@ class ProfileController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('agent');
     }
 
     public function index()
